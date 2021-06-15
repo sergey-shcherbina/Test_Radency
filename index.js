b40082c5adba6eda030f9b25fde535b10db69892
sergey <sergeyshc05@gmail.com>
2021-06-15 21:57:48 +0300

    second commit

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
 const chooseBestDistance = (t, k, ls) => {
-  let   distance, bestDistance = 0, ind_s = []
+  let distance, bestDistance = 0, ind_s = [];
   if (k <= ls.length) {
     for (let i = 0; i < k; i++) {
-      ind_s.push(i)
+      ind_s.push(i);
     }
-    let j = k - 1 
+    let j = k - 1; 
     while (j >= 0) {
-      distance = 0
+      distance = 0;
       for (let i = 0; i < k; i++) {
-        distance += ls[ind_s[i]] 
+        distance += ls[ind_s[i]]; 
       }
-      if (distance > bestDistance && distance <= t) bestDistance = distance
-      ind_s[k - 1] === ls.length - 1 ? j-- : j = k - 1
-      if( k === ls.length) break
+      if (distance > bestDistance && distance <= t) bestDistance = distance;
+      if( k === ls.length) break;
+      ind_s[k - 1] === ls.length - 1 ? j-- : j = k - 1;
       if (j >= 0) {
         for (let i = k - 1; i >= j; i--) {
-          ind_s[i] = ind_s[j] + i - j + 1 
+          ind_s[i] = ind_s[j] + i - j + 1 ;
         }
       }
     }
   } 
-  if (bestDistance) return bestDistance 
-  return null
+  if (bestDistance) return bestDistance;
+  return null;
 } 
-document.write(chooseBestDistance(174, 3, [51, 56, 58, 59, 61]))
-
+document.write(chooseBestDistance(174, 3, [51, 56, 58, 59, 61]));
+document.write('<br>' + chooseBestDistance(163, 3, [50]));
 
